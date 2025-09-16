a157c24b8e0d4c53bdb78d8b44a7bfe1476e7446
ToughGuyClub(laptab) <gyudong6185@gmail.com>
2025-09-16 15:04:17 +0900

    사각운동 함수 완성

---
+++ Lecture04_2D_Rendering/Drill3.py
@@ -0,0 +1,72 @@
+from pico2d import *
+import math
+open_canvas()
+
+boy = load_image('character.png')
+grass = load_image('grass.png')
+
+
+def move_right( x ):
+    print('moving right')
+    for i in range(70):
+        x += 5
+        draw_boy(x, 90)
+
+    pass
+
+
+def move_top():
+    print('moving top')
+    for y in range(90,550,5):
+        draw_boy(750,y)
+    pass
+
+
+def move_left():
+    print('moving left')
+    for x in range(750,50,-5):
+        draw_boy(x,550)
+    pass
+
+
+def move_bottom():
+    print('moving bottom')
+    for y in range(550,90,-5):
+        draw_boy(50, y)
+    pass
+
+
+def move_rectangle():
+    print("move rectangle")
+    move_right(400)
+    move_top()
+    move_left()
+    move_bottom()
+    move_right(50)
+    pass
+
+
+def move_circle():
+    print("move circle")
+    r=210
+    for deg in range(270, -90,-1):
+        x = 400 + r * math.cos(math.radians(deg))
+        y = 300 + r * math.sin(math.radians(deg))
+        draw_boy(x, y)
+    pass
+
+
+def draw_boy(x: float, y: float):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.01)
+
+
+while True:
+    move_circle()
+    move_rectangle()
+    break
+    pass
+
+
+close_canvas()
